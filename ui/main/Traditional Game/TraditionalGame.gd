635c3306d636bc1adf5fec646dcbe4d064a0a61f
jordanneumann7 <jordanneumann777@gmail.com>
2024-04-23 15:07:45 -0500

    Fixing some bugs after pulling

--- ui/main/Traditional Game/TraditionalGame.gd
+++ ui/main/Traditional Game/TraditionalGame.gd
@@ -35,20 +35,21 @@ func _process(delta):
 	pass
 
 func nextPlayer():
-	#sets the text of the scoreboard to the clicked number
-	sbArray[currentPlayer].get_children()[currentFrame].set_text(str(tempScoreHolder))
-	#adds clicked number to player's total score
-	totals[currentPlayer] += tempScoreHolder
-	#sets the text of the total score
-	get_node("Scoreboard/TotalScoresContainer").get_child(currentPlayer).set_text(str(totals[currentPlayer]))
-	tempScoreHolder = 0
-	currentPlayer += 1
-	
+	if(currentFrame != 10): #Added to prevent clicking the target after the last frame from crashing the program
+		#sets the text of the scoreboard to the clicked number
+		sbArray[currentPlayer].get_children()[currentFrame].set_text(str(tempScoreHolder))
+		#adds clicked number to player's total score
+		totals[currentPlayer] += tempScoreHolder
+		#sets the text of the total score
+		get_node("Scoreboard/TotalScoresContainer").get_child(currentPlayer).set_text(str(totals[currentPlayer]))
+		tempScoreHolder = 0
+		currentPlayer += 1
+		
 		#Goes to next frame after last player
 	if(currentPlayer > playerList.size() - 1):
 		currentPlayer = 0
 		currentFrame += 1
-	
+		
 	#Use this to check who wins after frame 10
 	if (currentFrame >= 10):
 		var winnerscore = totals.max()
@@ -62,9 +63,10 @@ func nextPlayer():
 
 #Used for updating the box style of a player
 func UpdateCurrentStyle(player, frame, style):
-	var currentsbRow = "Scoreboard/ScoreBoardRow" + str(player + 1)
-	get_node(currentsbRow).get_child(0).set("theme_override_styles/read_only", style)
-	get_node(currentsbRow).get_child(1).get_children()[frame].set("theme_override_styles/read_only", style)
+	if(currentFrame != 10): #Added to prevent clicking undo after the last frame from crashing the program
+		var currentsbRow = "Scoreboard/ScoreBoardRow" + str(player + 1)
+		get_node(currentsbRow).get_child(0).set("theme_override_styles/read_only", style)
+		get_node(currentsbRow).get_child(1).get_children()[frame].set("theme_override_styles/read_only", style)
 	
 #Finds the previous player, even across frames
 #Returns a list: [prev player index, prev frame]
@@ -112,21 +114,22 @@ func _on_miss_button_button_down():
 	nextPlayer()
 
 func _on_undo_button_button_down():
-	#finds prev player
-	var prev = findPreviousPlayer()
-	#Changes styles to reflect current player
-	UpdateCurrentStyle(currentPlayer, currentFrame, styleForNormal)
-	UpdateCurrentStyle(prev[0], prev[1], styleForCurrent)
-	#Updates current variables
-	currentPlayer = prev[0]
-	currentFrame = prev[1]
-	#finds the point that was last clicked
-	var lastPoint = int(sbArray[currentPlayer].get_children()[currentFrame].get_text())
-	#Updates total
-	totals[currentPlayer] -= lastPoint
-	get_node("Scoreboard/TotalScoresContainer").get_child(currentPlayer).set_text(str(totals[currentPlayer]))
-	#Updates scoreboard text
-	sbArray[currentPlayer].get_children()[currentFrame].set_text("")
+	if(currentFrame != 10): #Added to prevent clicking undo after the last frame from crashing the program
+		#finds prev player
+		var prev = findPreviousPlayer()
+		#Changes styles to reflect current player
+		UpdateCurrentStyle(currentPlayer, currentFrame, styleForNormal)
+		UpdateCurrentStyle(prev[0], prev[1], styleForCurrent)
+		#Updates current variables
+		currentPlayer = prev[0]
+		currentFrame = prev[1]
+		#finds the point that was last clicked
+		var lastPoint = int(sbArray[currentPlayer].get_children()[currentFrame].get_text())
+		#Updates total
+		totals[currentPlayer] -= lastPoint
+		get_node("Scoreboard/TotalScoresContainer").get_child(currentPlayer).set_text(str(totals[currentPlayer]))
+		#Updates scoreboard text
+		sbArray[currentPlayer].get_children()[currentFrame].set_text("")
 
 func _on_new_game_button_button_down():
 	get_tree().change_scene_to_file("res://ui/main/Traditional Game/TraditionalGame.tscn")
