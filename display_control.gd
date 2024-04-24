a9e61344f634973bb2428e73d7529e8e965c7542
jordanneumann7 <jordanneumann777@gmail.com>
2024-04-24 15:33:15 -0500

    Forgot to save before last push lol, this one is good

--- display_control.gd
+++ display_control.gd
@@ -4,7 +4,6 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	set("res://display/window/stretch/mode", "canvas_items")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
