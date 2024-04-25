411e83940d1e41357932a1630624df346713ec91
Endemacha <endemacha@gmail.com>
2024-04-25 11:18:06 -0500

    Added Connect 4(Script WIP)

---
+++ Connect4/Connect4Game.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_main_menu_pressed():
+	pass # Replace with function body.
+
+
+func _on_reset_pressed():
+	pass # Replace with function body.
